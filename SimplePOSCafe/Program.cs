6d624969f47e159418c9466db0cbd9a2f9df1105
leilaIT <delarealeila@gmail.com>
2023-11-22 17:15:00 +0800

    Add project files.

---
+++ SimplePOSCafe/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePOSCafe
+{
+    internal class Program
+    {
+        //*create a constructor for each food category
+        //with parameter string fileName (this is the file to be read)
+        //in constructor method, put all the code in "reading CSV file"
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string[]>> menu = new Dictionary<string, List<string[]>>();
+            List<string[]> drinks = new List<string[]>();
+            string[] tempArr = new string[] { };
+            string tempWord = "";
+
+            //store food items from CSV to program
+            List<food_items> foods = new List<food_items>();
+            foods.Add(new food_items("Alldrinks.csv"));
+            foods.Add(new food_items("Alldesserts.csv"));
+
+            //display all food items
+            foreach (food_items item in foods)
+                item.displayItem();
+
+            Console.ReadKey();
+            //reading csv file
+            using (StreamReader sr = new StreamReader("Alldrinks.csv"))
+            {
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    tempArr = line.Split(',');
+                    //remove space from word
+                    for (int x = 0; x < tempArr.Length; x++)
+                    {
+                        if (tempArr[x][0] == ' ') //if first letter is space
+                        {
+                            tempWord = tempArr[x];
+                            tempArr[x] = "";
+                            for (int i = 1; i < tempWord.Length; i++)
+                                tempArr[x] += tempWord[i];
+                        }
+                    }
+                    drinks.Add(tempArr);
+                }
+            }
+
+        }
+    }
+}
---
+++ SimplePOSCafe/food items.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePOSCafe
+{
+    internal class food_items
+    {
+        private List<string[]> items = new List<string[]>();
+        private string[] tempArr = new string[] { };
+        private string tempWord = "";
+        public food_items(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    tempArr = line.Split(',');
+                    //remove space from word
+                    for (int x = 0; x < tempArr.Length; x++)
+                    {
+                        if (tempArr[x][0] == ' ') //if first letter is space
+                        {
+                            tempWord = tempArr[x];
+                            tempArr[x] = "";
+                            for (int i = 1; i < tempWord.Length; i++)
+                                tempArr[x] += tempWord[i];
+                        }
+                    }
+                    items.Add(tempArr);
+                }
+            }
+        }
+        public void displayItem()
+        {
+            for (int x = 0; x < items.Count; x++)
+            {
+                for (int y = 0; y < items[x].Length; y++)
+                {
+                    Console.WriteLine(items[x][y]);
+                }
+            }
+        }
+    }
+}
